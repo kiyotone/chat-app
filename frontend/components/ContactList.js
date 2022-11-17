96c7c7d366ab486c3574d1715588569e4e3ae2ba
kiyotone <kunwarkirtan36@gmail.com>
2022-11-17 20:39:46 +0545

    sec

---
+++ frontend/components/ContactList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ContactList() {
+  return (
+    <div className="bg-[#20232b]">
+
+        <div>ContactList</div>
+
+    </div>
+  )
+}
+
+export default ContactList
