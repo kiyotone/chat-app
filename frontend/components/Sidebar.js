1d88244fc62a4a9f4bc3182344962b064724ba0e
kiyotone <kunwarkirtan36@gmail.com>
2022-11-17 20:30:12 +0545

    sec

--- frontend/components/Sidebar.js
+++ frontend/components/Sidebar.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {TbMessages} from 'react-icons/tb';
 import SidebarBtn from './SidebarBtn';
+import {BiMessageSquareCheck} from 'react-icons/bi'
+import {BiBlock} from 'react-icons/bi'
+import {RiSpam3Line} from 'react-icons/ri'
+
 
 function Sidebar() {
 
     const sidebarButtons = [
         {
-        title:"All",icon: <TbMessages />
+        title:"Pinned",icon: <BiMessageSquareCheck />
             
         },
         {
@@ -15,9 +19,13 @@ function Sidebar() {
         },
         
         {
-            title:"All",icon: <TbMessages />
+            title:"Blocked",icon: <BiBlock />
                     
         },
+        {
+            title:"Spam",icon:<RiSpam3Line />
+        },
+        
     ]
 
   return (
@@ -26,7 +34,7 @@ function Sidebar() {
         <div className="flex flex-col items-center pt-5">
             {
                 sidebarButtons.map((btn)=>(
-                    <div className="pl-4 flex items-center w-[10rem] h-[4rem] text-gray-400 text-base hover:bg-yellow-200 rounded-md hover:text-black">
+                    <div className="pl-4 flex items-center w-[12rem] h-[4rem] text-gray-400 text-base hover:bg-yellow-200 rounded-3xl hover:text-black" key={btn.name+"d1"}>
                     <SidebarBtn title = {btn.title} icon = {btn.icon} />
                     </div>
                     ))
